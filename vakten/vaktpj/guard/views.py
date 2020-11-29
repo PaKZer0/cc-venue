c68f1efe2a3ea4d932528f67d2ac88d09e051b39
PaK Zer0 <dmusta1ne.pak@gmail.com>
2020-11-29 03:07:49 +0100

    Basic icecast proxy django view

--- vakten/vaktpj/guard/views.py
+++ vakten/vaktpj/guard/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from revproxy.views import ProxyView
 
-# Create your views here.
+import os
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the venue index.")
+
+class IcecastProxyView(ProxyView):
+    upstream = 'http://{}:{}/'.format(
+		os.getenv('ICECAST_SERVER_NAME', 'cc-venue_icecast_1'),
+		os.getenv('ICECAST_SERVER_PORT', '8000'))
