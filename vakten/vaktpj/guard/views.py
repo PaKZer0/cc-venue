524035881bb913b888662225a420e5d8b67fc3cf
PaK Zer0 <dmusta1ne.pak@gmail.com>
2020-12-02 00:59:56 +0100

    Fixed static volume and added start bootstrap theme with test view

--- vakten/vaktpj/guard/views.py
+++ vakten/vaktpj/guard/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.views.generic.base import TemplateView
+
 def index(request):
     return HttpResponse("Hello, world. You're at the venue index.")
+
+class HomePageView(TemplateView):
+
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
