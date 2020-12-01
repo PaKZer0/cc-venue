4a05cb6b71af9417333df4a8371b787b0bc0f326
PaK Zer0 <dmusta1ne.pak@gmail.com>
2020-12-01 20:50:54 +0100

    First models added with migration

--- vakten/vaktpj/guard/admin.py
+++ vakten/vaktpj/guard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Stream, TokenType, Token
 
 # Register your models here.
+
+admin.site.register(Stream)
+admin.site.register(TokenType)
+admin.site.register(Token)
--- vakten/vaktpj/guard/models.py
+++ vakten/vaktpj/guard/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+import uuid
 
 # Create your models here.
+
+class Stream(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=32)
+    description = models.TextField()
+    mountpoint = models.CharField(max_length=128)
+    run_ads = models.BooleanField(default=False)
+    max_clients = models.IntegerField(default=50)
+    # True: no payment required to access False: Payment required
+    free_stream = models.BooleanField(default=True)
+    # the users can pay more than the access fee
+    flexible_fee = models.BooleanField(default=True)
+    access_fee = models.DecimalField(default=0.0, decimal_places=2, 
+                    max_digits=4)
+
+
+class TokenType(models.Model):
+    type = models.CharField(max_length=32, unique=True)
+
+
+class Token(models.Model):
+    value = models.UUIDField(primary_key=True, 
+		default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token_type = models.ForeignKey(TokenType, on_delete=models.CASCADE)
