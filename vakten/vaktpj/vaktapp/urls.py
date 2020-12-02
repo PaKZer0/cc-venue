05f76c284d27038b78c44741539dd89fa5dcf6c6
PaK Zer0 <dmusta1ne.pak@gmail.com>
2020-12-02 01:04:41 +0100

    Set guard app on the root url

--- vakten/vaktpj/vaktapp/urls.py
+++ vakten/vaktpj/vaktapp/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('guard/', include('guard.urls')),
+    path('', include('guard.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
